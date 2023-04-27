64fc6d50dbc6f49a629f55f668b150dba212862f
Raj kr. soni <72248764+bugRaj@users.noreply.github.com>
2023-04-28 01:28:22 +0530

    successfully uploaded

---
+++ Client/src/components/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "../scenes/Contact/Contact";
+import Footer from "./Footer";
+import Navbar from "../scenes/NavBar/Navbar";
+import WelcomePage from "../scenes/HomePage/WelcomePage";
+import Login from "../scenes/Login/Login";
+import Register from "../scenes/Register/Register";
+import PersonalMeal from "../scenes/PersonalMealPage/PersonalMeal";
+import Recipe from "../scenes/RecipeManagment/Recipe";
+import SingleRecipe from "../scenes/RecipeManagment/SingleRecipe";
+import MyMealPlans from "../scenes/MyMealPlans/MyMealPlans";
+import MealPlanDetails from "../scenes/MyMealPlans/MealPlanDetails";
+import Agora from "../scenes/VideoCalling/Agora";
+import ChatBot from "../scenes/ChatBot/ChatBot";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/meal" element={<PersonalMeal />} />
+          <Route path="/recipe" element={<Recipe />} />
+          <Route path="/recipe/:recipeId" element={<SingleRecipe />} />
+          <Route path="/support" element={<Contact />} />
+          <Route path="/myMealPlans" element={<MyMealPlans />} />
+          <Route path="/support/videoCall" element={<Agora />} />
+          <Route path="/chatBot" element={<ChatBot />} />
+          <Route path="/myMealPlans/:id" element={<MealPlanDetails />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

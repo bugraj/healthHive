00152e7d4248c664c8b8e788750c442727bf3905
Raj kr. soni <72248764+bugRaj@users.noreply.github.com>
2023-04-28 01:52:14 +0530

    upload index

---
+++ Server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+import mealRoutes from "./routes/meal.js";
+
+/* Configurations */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* ROUTES */
+
+app.use("/auth", authRoutes);
+app.use("/meal", mealRoutes);
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(process.env.MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`Didn't connected : ${error}`);
+  });
+
+
